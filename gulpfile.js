befcd11c09655cdb79a75689f7c25c845881039e
Leo Chen-Tsu_Lin <lin19890808@gmail.com>
2014-10-07 16:12:13 +0800

    init gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+ar gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+// plugins.jshint
+// plugins.imagemin
+// plugins.changed
+// plugins.notify
+// plugins.stylus
+// plugins.jsdoc
+// plugins.mocha
+// plugins.concat
+// plugins.uglify
+// plugins.if
+// plugins.exec
+// plugins.livereload
+// plugins.minifyCss
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+gulp.task('build', function() {
+
+});
