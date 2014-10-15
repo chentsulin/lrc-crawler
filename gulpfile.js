77d80bfd30f86345e17e14b233ca77ee56f602d2
Leo Chen-Tsu_Lin <lin19890808@gmail.com>
2014-10-15 14:58:23 +0800

    add gulp lint task

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
-ar gulp = require('gulp');
+var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
-// plugins.jshint
-// plugins.imagemin
+var jshint = plugins.jshint;
+var jsdoc  = plugins.jsdoc;
+var mocha  = plugins.mocha;
+
 // plugins.changed
 // plugins.notify
-// plugins.stylus
-// plugins.jsdoc
-// plugins.mocha
-// plugins.concat
-// plugins.uglify
 // plugins.if
 // plugins.exec
-// plugins.livereload
-// plugins.minifyCss
 
 gulp.task('default', function() {
   // place code for your default task here
 });
 
-gulp.task('build', function() {
-
+gulp.task('lint', function() {
+    return gulp.src('./lib/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter('fail'));
 });
+
+gulp.task('build', [ 'lint' ]);
