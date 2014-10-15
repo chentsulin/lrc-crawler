772ab20c50be9e91a48a971f71863fe80387dc96
Leo Chen-Tsu_Lin <lin19890808@gmail.com>
2014-10-15 17:55:59 +0800

    add chai & index.js

---
+++ test/lrc-crawler.js
@@ -0,0 +1,18 @@
+var crawler = require('..');
+var should  = require('chai').should();
+
+describe('crawler', function() {
+
+    describe('#getList()', function() {
+
+        it('respond with matching songs', function() {
+
+        });
+
+    });
+
+    describe('#getLrc()', function() {
+
+    });
+
+});
