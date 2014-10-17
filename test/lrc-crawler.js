d551f96a16fd109d2bf1b7b3c42cce070c5add8b
Leo Chen-Tsu_Lin <lin19890808@gmail.com>
2014-10-17 12:54:25 +0800

    add test case

--- test/lrc-crawler.js
+++ test/lrc-crawler.js
@@ -1,12 +1,26 @@
 var crawler = require('..');
 var should  = require('chai').should();
+var sinon   = require('sinon');
 
 describe('crawler', function() {
 
     describe('#getList()', function() {
 
+        it('should call callback function', function(done) {
+
+            var callback = sinon.spy();
+
+            crawler.getList('七里香', function() {
+                callback();
+                done();
+            });
+
+            callback.called.should.be.true;
+        });
+
         it('respond with matching songs', function(done) {
-            crawler.getList('七里香',function(err, list) {
+
+            crawler.getList('七里香', function(err, list) {
 
                 should.not.exist(err);
                 should.exist(list);
